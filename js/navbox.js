c4879e108d5983c3392060c3c40add595d820a78
Asterelle <asterell@gmail.com>
2017-04-16 22:58:03 -0400

    gitpages urls are weird?

--- js/navbox.js
+++ js/navbox.js
@@ -1,8 +1,8 @@
 (function(){ 
 var nav = '<ul id="navlist">\
   <li class="name"><a href="/pw">Aster\'s PW Tools</a></li>\
-  <li class="link"><a href="/pw/geniecalculator">Genie Calculator</a></li>\
-  <li class="link"><a href="/pw/refiningsimulator">Refining Simulator</a></li>\
+  <li class="link"><a href="/pw/geniecalculator/index.html">Genie Calculator</a></li>\
+  <li class="link"><a href="/pw/refiningsimulator/index.html">Refining Simulator</a></li>\
   <li class="link"><a href="/pw/waravatar">War Avatar Calculator</a></li>\
   <li class="link"><a href="/pw/starchart">Star Chart Calculator</a></li>\
   <li class="link"><a href="/pw/elysiumlocations">Elysium NPCs</a></li>\
