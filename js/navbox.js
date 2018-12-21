19d81407e69ba3f0533bb18e1145e820efa2c252
Asterelle <asterell@gmail.com>
2018-12-20 20:30:14 -0500

    navbox was getting too long

--- js/navbox.js
+++ js/navbox.js
@@ -1,12 +1,13 @@
 (function(){ 
-var nav = '<ul id="navlist">\
-  <li class="name"><a href="/pw">Aster\'s PW Tools</a></li>\
-  <li class="link"><a href="/pw/geniecalculator/">Genie Calculator</a></li>\
-  <li class="link"><a href="/pw/refiningsimulator/">Refining Simulator</a></li>\
-  <li class="link"><a href="/pw/waravatar/">War Avatar Calculator</a></li>\
-  <li class="link"><a href="/pw/starchart/">Star Chart Calculator</a></li>\
-  <li class="link"><a href="/pw/homesteadnpcs/">Homestead NPCs</a></li>\
-  <li class="link"><a href="/pw/chienkun/">Chienkun Calculator</a></li>\
-  </ul>';
+var nav = '<div id="navlist">\
+  <div class="name"><a href="/pw">Aster\'s PW Tools</a></div>\
+  <div class="links">\
+  <div class="link"><a href="/pw/geniecalculator/">Genie</a></div>\
+  <div class="link"><a href="/pw/refiningsimulator/">Refining</a></div>\
+  <div class="link"><a href="/pw/waravatar/">War Avatar</a></div>\
+  <div class="link"><a href="/pw/starchart/">Star Chart</a></div>\
+  <div class="link"><a href="/pw/homesteadnpcs/">Homestead</a></div>\
+  <div class="link"><a href="/pw/chienkun/">Chienkun</a></div>\
+  </div></div>';
 document.write(nav);
 })()
